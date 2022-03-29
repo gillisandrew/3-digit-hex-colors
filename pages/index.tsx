f4ef0e32a48411bef541abdb230b0e8bd66b0281
Andrew Gillis <gillis.andrew@gmail.com>
2022-03-29 16:54:42 -0400

    add logo element

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,30 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { MouseEventHandler } from "react";
+import { MouseEventHandler, useEffect, useState } from "react";
 import useCopyToClipboard from "../hooks/useCopyToClipboard";
 import styles from "../styles/Home.module.css";
 
 const hex = "0123456789ABCDEF".split("");
 const Home: NextPage = () => {
+  const [rotation, setRotation] = useState(0)
   const [_value, copy] = useCopyToClipboard();
-  const clickHandler: MouseEventHandler<HTMLButtonElement>  = (e) => {
+  const copyHandler: MouseEventHandler<HTMLButtonElement>  = (e) => {
     copy(e.currentTarget.title)
     e.currentTarget.blur()
   }
+
+  useEffect(() => {
+    const listener = (e: KeyboardEvent) => {
+      if(e.key === 'Enter') {
+        setRotation((rotation + 1) % 6)
+      }
+    }
+    document.addEventListener('keydown', listener)
+    return () => {
+      document.removeEventListener('keydown', listener)
+    }
+  }, [rotation])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -18,16 +32,27 @@ const Home: NextPage = () => {
       </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>3-Digit Hex Colors</h1>
-        <p className={styles.description}>Click on the color to copy</p>
+        <p className={styles.description}>Click on the color to copy.</p>
+        <p className={styles.tip}>Press <kbd>Enter</kbd> to reorder them.</p>
         <div className={styles.tiles}>
           {hex.map((i) =>
             hex.map((j) =>
               hex.map((k) => {
-                const color = `#${i}${j}${k}`;
+                const color = ((r) =>{
+                  switch(r) {
+                    case 0: return `#${i}${j}${k}`
+                    case 1: return `#${i}${k}${j}`
+                    case 2: return `#${j}${i}${k}`
+                    case 3: return `#${j}${k}${i}`
+                    case 4: return `#${k}${j}${i}`
+                    case 5: return `#${k}${i}${j}`
+                  }
+                })(rotation);
+
                 return (
                   <button
-                    key={color}
-                    onClick={clickHandler}
+                    key={i+j+k}
+                    onClick={copyHandler}
                     className={styles.tile}
                     style={{background: color}}
                     title={color}
