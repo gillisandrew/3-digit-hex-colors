f4ec4dc2934a7ba1d6de74637dd529ad662a4b75
gillisandrew <gillis.andrew@gmail.com>
2022-03-29 18:10:13 +0000

    Deploying to gh-pages from  @ 44f5b58f05517c7b3db687199b6b501dc117db10 🚀

---
+++ _next/static/wBY-PKEyyyi_pcjMYN5WI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/d81abc5cabf42f3d.css","static/chunks/pages/index-44a3eb5ece279991.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
