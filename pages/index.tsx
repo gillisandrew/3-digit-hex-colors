7725c4e4cc77fe413372cb0457af9c01b80e6766
Andrew Gillis <gillis.andrew@gmail.com>
2022-04-05 17:49:13 -0400

    clean

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,58 +5,81 @@ import useCopyToClipboard from "../hooks/useCopyToClipboard";
 import styles from "../styles/Home.module.css";
 
 const hex = "0123456789ABCDEF".split("");
-const mapRotationToPermutation = (r: number, i: string, j: string, k: string) =>{
-  switch(r) {
-    case 0: return `#${i}${j}${k}`
-    case 1: return `#${i}${k}${j}`
-    case 2: return `#${j}${i}${k}`
-    case 3: return `#${j}${k}${i}`
-    case 4: return `#${k}${j}${i}`
-    case 5: return `#${k}${i}${j}`
+const mapRotationToPermutation = (
+  r: number,
+  i: string,
+  j: string,
+  k: string
+) => {
+  switch (r) {
+    case 0:
+      return `#${i}${j}${k}`;
+    case 1:
+      return `#${i}${k}${j}`;
+    case 2:
+      return `#${j}${i}${k}`;
+    case 3:
+      return `#${j}${k}${i}`;
+    case 4:
+      return `#${k}${j}${i}`;
+    case 5:
+      return `#${k}${i}${j}`;
   }
-}
+};
 const Home: NextPage = () => {
-  const [rotation, setRotation] = useState(0)
+  const [rotation, setRotation] = useState(0);
   const [_value, copy] = useCopyToClipboard();
 
-  const copyHandler: MouseEventHandler<HTMLButtonElement>  = (e) => {
-    copy(e.currentTarget.title)
-    e.currentTarget.blur()
-  }
+  const copyHandler: MouseEventHandler<HTMLButtonElement> = (e) => {
+    copy(e.currentTarget.title);
+    e.currentTarget.blur();
+  };
 
   useEffect(() => {
     const listener = (e: KeyboardEvent) => {
-      if(e.key === 'Enter') {
-        setRotation((rotation + 1) % 6)
+      if (e.key === "Enter") {
+        setRotation((rotation + 1) % 6);
       }
-    }
-    document.addEventListener('keydown', listener)
+    };
+    document.addEventListener("keydown", listener);
     return () => {
-      document.removeEventListener('keydown', listener)
-    }
-  }, [rotation])
+      document.removeEventListener("keydown", listener);
+    };
+  }, [rotation]);
 
   return (
     <div className={styles.container}>
       <Head>
         <title>3-Digit Hex Colors</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="shortcut icon"
+          type="image/x-icon"
+          href="/3-digit-hex-colors/favicon.ico"
+        ></link>
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href="/3-digit-hex-colors/favicon.ico"
+        ></link>
       </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>3-Digit Hex Colors</h1>
         <p className={styles.description}>Click on the color to copy.</p>
-        <p className={styles.tip}>Press <kbd>Enter</kbd> to reorder them.</p>
+        <p className={styles.tip}>
+          Press <kbd>Enter</kbd> to reorder them.
+        </p>
         <div className={styles.tiles}>
           {hex.map((i) =>
             hex.map((j) =>
               hex.map((k) => {
-                const color = mapRotationToPermutation(rotation,i,j,k);
+                const color = mapRotationToPermutation(rotation, i, j, k);
                 return (
                   <button
-                    key={i+j+k}
+                    key={i + j + k}
                     onClick={copyHandler}
                     className={styles.tile}
-                    style={{background: color}}
+                    style={{ background: color }}
                     title={color}
                   />
                 );
